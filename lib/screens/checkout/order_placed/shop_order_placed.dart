457380aaea7d882c1f2ad3b52e6fdc61e55f546a
Ujwal <ujwalmv36@gmail.com>
2023-05-05 16:32:08 +0530

    add WillpopScope

--- lib/screens/checkout/order_placed/shop_order_placed.dart
+++ lib/screens/checkout/order_placed/shop_order_placed.dart
@@ -12,70 +12,76 @@ class ShopOrderPlaced extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.scaffoldBlue,
-      appBar: AppBar(
-        backgroundColor: AppColors.white,
-        elevation: 0,
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.arrow_back,
-              color: AppColors.black,
-            )),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              color: AppColors.white,
-              child: Column(
-                children: [
-                  CircleAvatar(
-                      radius: 15,
-                      backgroundColor: AppColors.primaryGreen,
-                      child: Icon(Icons.check,color: AppColors.white,)),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Order Placed Successfully'",
-                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        height: 1.445,
-                        color: AppColors.primaryGreen),
-                  ),
-                  Text(
-                    "You can see your booking in my bookings",
-                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                      height: 1.445,
-                      color: const Color(0xff333333),
+    return WillPopScope(
+      onWillPop: () async {
+         Navigator.push(context, MaterialPageRoute(builder: (context) => const BottomBarScreen(),));
+         return true;
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.scaffoldBlue,
+        appBar: AppBar(
+          backgroundColor: AppColors.white,
+          elevation: 0,
+          leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_back,
+                color: AppColors.black,
+              )),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                color: AppColors.white,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                        radius: 15,
+                        backgroundColor: AppColors.primaryGreen,
+                        child: Icon(Icons.check,color: AppColors.white,)),
+                    const SizedBox(
+                      height: 10,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  AppButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const MyOrderServices(),));
-                    },
-                    text: "Go to my Order",
-                    color: AppColors.primaryGreen,
-                    txtClr: AppColors.white,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                ],
-              ),
-            )
-          ],
+                    Text(
+                      "Order Placed Successfully'",
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          height: 1.445,
+                          color: AppColors.primaryGreen),
+                    ),
+                    Text(
+                      "You can see your booking in my bookings",
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        height: 1.445,
+                        color: const Color(0xff333333),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    AppButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const MyOrderServices(),));
+                      },
+                      text: "Go to my Order",
+                      color: AppColors.primaryGreen,
+                      txtClr: AppColors.white,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
